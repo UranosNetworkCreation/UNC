262eb519310a9ecc329c36dd6a36afe97e76d9cc
Apilonius64 <HenryFell@outlook.de>
2022-11-03 11:15:40 +0100

    Start programming PluginLoader (not tested)

---
+++ kernel/PluginLoader/PluginLoader.cpp
@@ -0,0 +1,67 @@
+#include "PluginLoader.h"
+#include <NativeScript.hpp>
+#include <Ref.hpp>
+#include <Variant.hpp>
+
+using namespace godot;
+
+PluginLoader::PluginLoader() {
+    // Init member vars
+    DirectoryInst = new Directory();
+
+    // Get global instances
+    ResLInst    = ResourceLoader::get_singleton();
+    OSInst      = OS            ::get_singleton();
+
+    // Load plugins
+    loadPlugins();
+}
+
+PluginLoader::~PluginLoader() {
+    // clean up plugin array (-> "std::vector<NetworkKernelPlugin*> plugins")
+    unloadPlugins();
+
+    // clean up member vars
+    delete DirectoryInst;
+}
+
+void PluginLoader::loadPlugins() {
+    Godot::print("[PluginLoader] Load NNKernelPlugins ...");
+
+    // Setup DirectoryInst
+    DirectoryInst->open("res://gdns/plugins/");
+    DirectoryInst->list_dir_begin();
+
+    while(true) {
+        // Get current file str
+        String file = DirectoryInst->get_next();
+
+        // Exit if all files are processed
+        if(file == "") {
+            break;
+        }
+
+        // Only "non-hidden" progress files
+        else if(!file.begins_with(".")) {
+            // Load Script
+            Ref<NativeScript> plugin = ResLInst->load("res://gdns/plugins/" + file);
+            plugins.push_back(plugin->new_());
+        }
+    }
+}
+
+void PluginLoader::unloadPlugins() {
+    // ..
+}
+
+std::vector<NetworkKernelPluginInfo> PluginLoader::getAllPluginsInfo() {
+    // Container for result
+    std::vector<NetworkKernelPluginInfo> result;
+
+    // Iterate over plugins
+    for(std::vector<NetworkKernelPlugin*>::iterator it = plugins.begin(); it != plugins.end(); ++it) {
+        result.push_back((*it)->getInfo());
+    }
+
+    return result;
+}
---
+++ kernel/PluginLoader/PluginLoader.h
@@ -0,0 +1,31 @@
+#ifndef PLUGINLOADER_H_UNC_KERNEL
+#define PLUGINLOADER_H_UNC_KERNEL
+
+#include <vector>
+#include <Godot.hpp>
+#include <Directory.hpp>
+#include <Array.hpp>
+#include <ResourceLoader.hpp>
+#include <OS.hpp>
+#include "../../plugins/NetworkKernelPlugin.h"
+
+namespace godot {
+    class PluginLoader : godot::Object {
+    GODOT_CLASS(PluginLoader, godot::Object)
+    private:
+        std::vector<NetworkKernelPlugin*> plugins;
+        Directory* DirectoryInst;
+        ResourceLoader* ResLInst;
+        OS* OSInst;
+    public:
+        PluginLoader();
+        ~PluginLoader();
+        void loadPlugins();
+        void unloadPlugins();
+        std::vector<NetworkKernelPluginInfo> getAllPluginsInfo();
+
+        static void _register_methods();
+    };
+}
+
+#endif
