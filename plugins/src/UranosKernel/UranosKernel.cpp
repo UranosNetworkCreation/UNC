a2abc30b50fc10b9b327e1e0df84ff0da81755f0
Apilonius64 <HenryFell@outlook.de>
2022-12-15 14:39:04 +0100

    Upload GDN comments

--- plugins/src/UranosKernel/UranosKernel.cpp
+++ plugins/src/UranosKernel/UranosKernel.cpp
@@ -6,26 +6,33 @@ using namespace godot;
 
 NetworkKernelPluginInfo UranosKernel::getInfo() {
     return NetworkKernelPluginInfo(
-        "UranosKernel",
-        "0.0.1",
-        "Apilonius64",
-        "Default network kernel"
+        "UranosKernel",         // name
+        "0.0.1",                // version
+        "Apilonius64",          // author
+        "Default network kernel"// description
     );
 }
 
+// GD-Constructur
 void UranosKernel::_init() {
 
 }
 
 void UranosKernel::_register_methods() {
+    // Register GD constructur
     register_method("_init", &UranosKernel::_init);
 }
 
 void UranosKernel::Layer::build(size_t n_size, size_t parent_size, int64_t seed)  {
     size = n_size;
+
+    // Create rng
     Ref<RandomNumberGenerator> rnd = RandomNumberGenerator::_new();
+
+    // Create empty weights array
     Array Nweights = Array();
 
+    // Init rng
     if(seed == -1) {
         Godot::print("   | prepare pseudo-rnd: randomize seed ...");
         rnd->randomize();
@@ -35,6 +42,7 @@ void UranosKernel::Layer::build(size_t n_size, size_t parent_size, int64_t seed)
         rnd->set_seed(seed);
     }
     
+    // Randomize weights
     for(size_t i = 0; i < size;i++) {
         PoolRealArray randomizedArr;
         std::stringstream msg;
@@ -51,19 +59,27 @@ void UranosKernel::Layer::build(size_t n_size, size_t parent_size, int64_t seed)
 #endif
         Nweights.push_back(randomizedArr);
     }
+
+    // Build Layer
     buildFromWeights(Nweights, n_size, parent_size);
 }
 
 void UranosKernel::Layer::buildFromWeights(Array n_weights, size_t n_size, size_t parent_size) {
     Godot::print(" | Build Layer ...");
+    // Update vars
     weights = n_weights;
     size = n_size;
     parentSz = parent_size;
 }
 
 PoolRealArray UranosKernel::Layer::getResult(NeuronActivationFunc activation_func, PoolRealArray input) {
+    // Update lastInput
     lastInput = input;
+
+    // Create result array
     PoolRealArray result;
+
+    // Do activation_func(wi * conn, ...) for each neuron
     for(int i=0;i<size;i++) {
         float neuronInputValue = 0;
 #ifdef UNC_EXTENDED_DEBUG
@@ -83,6 +99,7 @@ PoolRealArray UranosKernel::Layer::getResult(NeuronActivationFunc activation_fun
         result.append(activation_func(neuronInputValue));
     }
 
+    // Update lastresult
     lastResult = result;
 
     return result;
@@ -95,11 +112,14 @@ PoolRealArray UranosKernel::Layer::backprop(
     bool outputLayer,
     real_t learning_rate
 ) {
+    // Create empty array for the gradient
     PoolRealArray gradient;
+    // Check if it's an oututLayer
     if(!outputLayer) {
 #ifdef UNC_EXTENDED_DEBUG
         Godot::print(" | Using algorithm for hidden layer ...");
 #endif
+        // Calculate the gradient foreach neuron
         for (int idx = 0; idx < size; idx++) {
             real_t sum = 0;
 #ifdef UNC_EXTENDED_DEBUG
@@ -110,9 +130,11 @@ PoolRealArray UranosKernel::Layer::backprop(
 #ifdef UNC_EXTENDED_DEBUG
                 Godot::print("  | Update sum for idx " + STR(ci));
 #endif
+                // Add to sum
                 sum += ((PoolRealArray)under_weights[ci])[idx] * under_grad[ci];                                                                             
             }
 
+            // push back calculated result
             gradient.push_back(sum * derivative(lastResult[idx]));
         }
     }
@@ -120,10 +142,12 @@ PoolRealArray UranosKernel::Layer::backprop(
 #ifdef UNC_EXTENDED_DEBUG
         Godot::print(" | Using algorithm for output layer ...");
 #endif
+        // Calculate th egradient foreach neuron
         for (int idx = 0; idx < size; idx++) {
 #ifdef UNC_EXTENDED_DEBUG
             Godot::print("  | Calc gradient for idx " + STR(idx));
 #endif
+            // push the calculated result back
             gradient.push_back((lastResult[idx] - under_grad[idx]) * derivative(lastResult[idx]));
         }
     }
@@ -138,26 +162,30 @@ PoolRealArray UranosKernel::Layer::backprop(
     for (int idx = 0; idx < size; idx++) {
         PoolRealArray connWeights;
         for(int i = 0; i < parentSz; i++) {
-            // POSSIBLE WRONG
 #ifdef UNC_EXTENDED_DEBUG
             Godot::print("   | Calc new weight for " + STR(idx) + " | " + STR(i));
             Godot::print("   | lastInputV ...");
 #endif
+            // Get last input
             real_t lastInputV = lastInput[i];
 #ifdef UNC_EXTENDED_DEBUG
             Godot::print("   | gradientV ...");
 #endif
+            // Get gradient
             real_t gradientV = gradient[idx];
 #ifdef UNC_EXTENDED_DEBUG
             Godot::print("   | calc final ...");
 #endif
+            // Calculate nValue
             real_t nValue = ((PoolRealArray)weights[idx])[i] - (learning_rate * lastInputV * gradientV);
 #ifdef UNC_EXTENDED_DEBUG
             Godot::print("   | push ...");
 #endif
+            // push back to weights array
             connWeights.push_back(nValue);
         }
 
+        // push back to new weights
         nWeights.push_back(connWeights);
     }
 #ifdef UNC_EXTENDED_DEBUG
@@ -165,12 +193,14 @@ PoolRealArray UranosKernel::Layer::backprop(
     Godot::print(" | Notice: Shape of nWeights: " + STR(nWeights.size()) + " | " + STR(((PoolRealArray)nWeights[0]).size()));
     Godot::print(" | Notice: Shape of weights: " + STR(weights.size()) + " | " + STR(((PoolRealArray)weights[0]).size()));
 #endif
+    // Update weights
     weights = nWeights;
 
 #ifdef UNC_EXTENDED_DEBUG
     Godot::print(" | weights updated.");
     Godot::print(" | Notice: Shape of weights: " + STR(weights.size()) + " | " + STR(((PoolRealArray)weights[0]).size()));
 #endif
+    // return the gradient
     return gradient;
 }
 
@@ -184,22 +214,29 @@ int UranosKernel::buildLayer(
     Array weights
 ) {
     Godot::print("[UNC-Kernel] Build Layer ...");
+    // Create the Layer
     Layer result;
+
+    // Check if weights are given
     if(!weights.empty()) {
         Godot::print("| Build from weights ...");
+        // Build Layer
         result.buildFromWeights(weights, size, parent_size);
     }
     else {
         Godot::print("| Gen weights ...");
+        // Build Layer
         result.build(size, parent_size);
     }
 
+    // push back to Layer-Array
     int idx = Layers.size();
     Layers.push_back(result);
 
     return idx;
 }
 
+// feeds forward
 PoolRealArray UranosKernel::SimpleLayerCall(
     int id,
     PoolRealArray input,
@@ -210,6 +247,7 @@ PoolRealArray UranosKernel::SimpleLayerCall(
 #endif
     NeuronActivationFunc aFunc;
 
+    // Set the activation func
     switch (activationFunc)
     {
     case ACTIVATION_FUNC_SIGMOID:
@@ -222,6 +260,7 @@ PoolRealArray UranosKernel::SimpleLayerCall(
         break;
     }
 
+    // Call the Layer and return the result
     return Layers[id].getResult(aFunc, input);
 }
 
@@ -238,6 +277,7 @@ PoolRealArray UranosKernel::BackpropLayer(
 #endif
     NeuronActivationFunc ADerivative;
 
+    // Set the activation func
     switch (activationFunc)
     {
     case ACTIVATION_FUNC_SIGMOID:
@@ -250,11 +290,13 @@ PoolRealArray UranosKernel::BackpropLayer(
         break;
     }
 
+    // Call the Layer and return the result
     return Layers[id].backprop(underGrad, underWeights, ADerivative, outputLayer, learning_rate);
 }
 
 Array UranosKernel::getLayerWeights(
     int id
 ) {
+    // Call the Layer and return the result
     return Layers[id].getWeights();
 }
--- plugins/src/UranosKernel/UranosKernelLib.cpp
+++ plugins/src/UranosKernel/UranosKernelLib.cpp
@@ -1,16 +1,20 @@
 #include "UranosKernel.h"
 
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
+    // Init GDNative API
     godot::Godot::gdnative_init(o);
 }
 
 extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *o) {
+    // Cleanup
     godot::Godot::gdnative_terminate(o);
 }
 
 extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
+    // init nativescript
     godot::Godot::nativescript_init(handle);
 
+    // register classes
     godot::register_class<godot::GDDataContainer>();
     godot::register_class<godot::NetworkKernelPluginInfo>();
     godot::register_class<godot::NetworkKernelPlugin>();
